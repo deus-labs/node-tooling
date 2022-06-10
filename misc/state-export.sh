c34b59df6d864500e07ad76e29f776347c447eb9
Furkan Kaymak <furkan@deuscrypto.fi>
2022-06-10 15:15:27 +0300

    state export script

---
+++ misc/state-export.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+USERNAME=${1:-juno}
+CHAIN_ID=${2:-juno-1}
+EXPORT_PATH=${2:-/junotools-snapshots}
+
+HOME=/home/"$USERNAME"
+. "$HOME"/.profile
+
+LATEST_BLOCK=$(junod status | jq -r '.SyncInfo.latest_block_height')
+EXPORT_FILENAME=export-"$CHAIN_ID"-"$LATEST_BLOCK".json
+
+systemctl stop cosmovisor 
+$DAEMON_NAME export --height "$LATEST_BLOCK" --home "$DAEMON_HOME" > "$EXPORT_PATH/$EXPORT_FILENAME"
+systemctl start cosmovisor
