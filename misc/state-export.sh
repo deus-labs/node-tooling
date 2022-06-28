11853599601dc8a1074808c35604e8c5ed98c2ce
Furkan Kaymak <furkan@deuscrypto.fi>
2022-06-28 15:33:20 +0300

    add parse to state export

--- misc/state-export.sh
+++ misc/state-export.sh
@@ -3,6 +3,8 @@
 USERNAME=${1:-juno}
 CHAIN_ID=${2:-juno-1}
 EXPORT_PATH=${2:-/junotools-snapshots}
+PARSER_PATH=${3:-$HOME/junotools-scripts}
+PARSER_COMMAND=${4:-npm run parser}
 
 HOME=/home/"$USERNAME"
 . "$HOME"/.profile
@@ -13,3 +15,5 @@ EXPORT_FILENAME=export-"$CHAIN_ID"-"$LATEST_BLOCK".json
 systemctl stop cosmovisor 
 su "$USERNAME" -c "$(which "$DAEMON_NAME") export --home $DAEMON_HOME > $EXPORT_PATH/$EXPORT_FILENAME"
 systemctl start cosmovisor
+
+cd "$PARSER_PATH" && echo "$PARSER_COMMAND $EXPORT_PATH/$EXPORT_FILENAME" | sh
