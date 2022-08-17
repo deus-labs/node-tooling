3e1d0776b73c4edf6f82a5a9d2235b9f2c7fc185
Furkan Kaymak <furkan@deuscrypto.fi>
2022-08-17 13:33:14 +0300

    fixes

--- misc/state-export.sh
+++ misc/state-export.sh
@@ -2,14 +2,14 @@
 
 USERNAME=${1:-juno}
 CHAIN_ID=${2:-juno-1}
-EXPORT_PATH=${2:-/junotools-snapshots}
-PARSER_PATH=${3:-$HOME/junotools-scripts}
-PARSER_COMMAND=${4:-npm run parser}
+EXPORT_PATH=${3:-/junotools-snapshots}
+PARSER_PATH=${4:-$HOME/junotools-scripts}
+PARSER_COMMAND=${5:-npm run parser}
 
 HOME=/home/"$USERNAME"
 . "$HOME"/.profile
 
-LATEST_BLOCK=$(junod status | jq -r '.SyncInfo.latest_block_height')
+LATEST_BLOCK=$("$DAEMON_NAME" status | jq -r '.SyncInfo.latest_block_height')
 EXPORT_FILENAME=export-"$CHAIN_ID"-"$LATEST_BLOCK".json
 
 systemctl stop cosmovisor 
@@ -17,3 +17,4 @@ su "$USERNAME" -c "$(which "$DAEMON_NAME") export --home $DAEMON_HOME > $EXPORT_
 systemctl start cosmovisor
 
 cd "$PARSER_PATH" && echo "$PARSER_COMMAND $EXPORT_PATH/$EXPORT_FILENAME" | sh
+
